d6280f4efdca87639ec052c5ca77719541c41ff3
sukh <sukh@gmail.om.com>
2023-08-01 14:24:57 +0530

    added k8s yml file

--- src/main/java/com/example/springapp/SpringAppApplication.java
+++ src/main/java/com/example/springapp/SpringAppApplication.java
@@ -1,13 +1,29 @@
-package com.example.springapp;
+// package com.example.springapp;
 
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class SpringAppApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(SpringAppApplication.class, args);
+// 	}
+
+// }
+
+// -----------------------
+
+package com.example.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
 public class SpringAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAppApplication.class, args);
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello User";
 	}
-
 }
